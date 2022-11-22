3ad663bf5b138ee1020898f6dc6e801f25c64877
Onyanov Dmitrii <onyanovd@mail.ru>
2022-11-22 17:50:33 +0500

    Lesson 02

---
+++ 015/Program.cs
@@ -0,0 +1,22 @@
+﻿// 15. С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет 
+// (Вывести: NO).
+int n=Convert.ToInt32(Console.ReadLine());
+n=Math.Abs(n);
+//Вариант 1 - простой - последняя с конца 4123/100%10 - 1
+/*if (n<100) System.Console.WriteLine("No");
+System.Console.WriteLine(n/100%10);
+*/
+
+//Вариант 2 - 12345678 - 3
+if (n>99)
+{
+    while (n>1000)
+    {
+        n=n/10;
+    }
+    System.Console.WriteLine(n%10);
+}
+else
+{
+    System.Console.WriteLine("No");
+}
---
+++ 017/Program.cs
@@ -0,0 +1,8 @@
+﻿// 17. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+int[] array = new int[6];
+// Каждому индексу присваиваем значение
+string[] weekDays = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+System.Console.WriteLine("Введите число от 0 до 6: ");
+int a=Convert.ToInt32 (System.Console.ReadLine());
+Console.WriteLine(weekDays[a]);
+if (a==0 || a==6) System.Console.WriteLine("YES!!! ВЫХОДНОЙ!!!"); else System.Console.WriteLine("No");
